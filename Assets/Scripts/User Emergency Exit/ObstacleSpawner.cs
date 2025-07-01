334ef2ded2a6ec0c0bd200cb1d6766e3c86c44ba
c_espinoza <cre2121@columbia.edu>
2025-06-30 21:27:25 -0400

    made the spawning of the fires happen at random.

--- Assets/Scripts/User Emergency Exit/ObstacleSpawner.cs
+++ Assets/Scripts/User Emergency Exit/ObstacleSpawner.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using UnityEngine;
+using Random = System.Random;
 
 public class ObstacleSpawner : MonoBehaviour {
     // Start is called before the first frame update
-    public GameObject obstacle;
+    public GameObject[] obstacle;
     public GeneratePath user;
     
     // Prevent multiple spawns
     private bool isSpawning = false; 
+    
+    // Random Generator
+    private readonly Random rnd = new Random();
 
     private void Update() {
+        // Deactivate all obstacles
+        foreach (var obj in obstacle) {
+            obj.SetActive(false);
+        }
+        
+        // Randomly select a obstacle to set 
         if (!isSpawning && obstacle is not null  && user) {
             StartCoroutine(PeriodicSpawn());
             isSpawning = true;
@@ -17,19 +27,16 @@ public class ObstacleSpawner : MonoBehaviour {
     }
 
     private IEnumerator PeriodicSpawn() {
-        // Wait for obstacle to be recognized
-        yield return new WaitForSeconds(1);
+        // Wait for at least a 1 second
+        //int wait = rnd.Next(1, 10);
+        var wait = 1;
+        yield return new WaitForSeconds(wait);
         
-        while (true) {
-            // Keep the obstacle hidden for 3 seconds
-            obstacle.SetActive(false);
-            yield return new WaitForSeconds(3);
-
-            // Unhide the obstacle for 15 seconds
-            obstacle.SetActive(true);
-            yield return new WaitForSeconds(1);
-            user.UpdatePath(obstacle.transform); // Update path for user
-            yield return new WaitForSeconds(14);
-        }
+        // pick a random obstacle
+        //var randomObstacle = rnd.Next(0, obstacle.Length);
+        var randomObstacle = 0;
+        GameObject selectedObstacle = obstacle[randomObstacle];
+        selectedObstacle.SetActive(true);
+        user.UpdatePath(selectedObstacle.transform);
     }
 }
