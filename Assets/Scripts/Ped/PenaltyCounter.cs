df28cfd09e7796fa3c93d43da89e097dcda85627
c_espinoza <cre2121@columbia.edu>
2025-07-13 19:54:26 -0400

    penalty counter now logs data when exiting simulation

--- Assets/Scripts/Ped/PenaltyCounter.cs
+++ Assets/Scripts/Ped/PenaltyCounter.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using UnityEngine;
 
 public class PenaltyCounter : MonoBehaviour {
@@ -5,6 +7,34 @@ public class PenaltyCounter : MonoBehaviour {
     [Header("Penalty Counters")]
     public int collisionWithFire, collisionWithPedestrian;
     
+    [Header("Log Settings")]
+    public string LogPath = "C:/Users/tower/Documents/Unity Projects/SubwayNav/Assets/Logs/" ;
+    private StreamWriter writer;
+
+    private void Start() {
+        // Create a new file name
+        string fileName = "penalty_counter_" + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".csv";
+        
+        // Create a new streamwriter
+        try {
+            writer = new StreamWriter(LogPath + fileName);
+        } catch (DirectoryNotFoundException) {
+            Debug.LogError($"Penalty Counter: Log Path location doesn't exits. Please change it. ");
+        } catch (Exception e) {
+            Debug.LogError($"{e.Message}");
+        }
+        
+        // Write column names
+        writer.WriteLineAsync("Collision with Fire, Collision with Pedestrian" + Environment.NewLine);
+    }
+
+    private void OnApplicationQuit() {
+        // Log data
+        writer.WriteLine($"{collisionWithFire}, {collisionWithPedestrian}");
+        writer.Close();
+        Debug.Log("Penalty Counter: Log saved to: " + LogPath);
+    }
+
     // Increase counters it collides with a fire or 
     private void OnTriggerEnter(Collider other) {
         var layer = other.gameObject.layer;
