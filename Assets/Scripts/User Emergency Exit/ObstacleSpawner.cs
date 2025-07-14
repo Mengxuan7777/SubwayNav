9b59d3d83ab93a4c4e60b74b4ea88ea1e4eb248f
c_espinoza <cre2121@columbia.edu>
2025-07-13 22:43:01 -0400

    implemented the ability to predefine the fires that spawns and it intensity.

--- Assets/Scripts/User Emergency Exit/ObstacleSpawner.cs
+++ Assets/Scripts/User Emergency Exit/ObstacleSpawner.cs
@@ -7,11 +7,21 @@ public class ObstacleSpawner : MonoBehaviour {
     [Header("Fire Locations")]
     public GameObject[] obstacles;
     
-    // Prevent multiple spawns
-    [Header("Spawn Settings")]
-    private bool isSpawning = false;
-    public int fireIndex = -1;
-    public float fireSize = 0;
+    [Header("Fire Spawn Parameters - Fire 1")]
+    public int fireIndex1 = -1;                 // Index of first fire to spawn
+    public float InitialFireSize1 = 0;          // Intensity it is initially set
+    public float UpdateFireSize1 = 0;           // Intensity fire is set on after update delay
+    public float fireSpawnDelay1 = 0.1f;             // Delay between start and the fire spawning
+    public float fireUpdateDelay1 = 0;            // Delay between the fire first spawning and the intensity being updated
+    public bool SizeUpdateIncremental1 = false; // Makes the increase in intensity happen slowly
+    
+    [Header("Fire Spawn Parameters - Fire 2")]
+    public int fireIndex2 = -1;                 // Index of second fire to spawn
+    public float InitialFireSize2 = 0;          // Intensity it is initially set
+    public float UpdateFireSize2 = 0;           // Intensity fire is set on after update delay
+    public float fireSpawnDelay2 = 0.1f;             // Delay between start and the fire spawning
+    public float fireUpdateDelay2 = 0;            // Delay between the fire first spawning and the intensity being updated
+    public bool SizeUpdateIncremental2 = false; // Makes the increase in intensity happen slowly
     
     // Random Generator
     private readonly Random rnd = new Random();
@@ -22,29 +32,48 @@ public class ObstacleSpawner : MonoBehaviour {
             obj.SetActive(false);
         }
         
-        // Randomly select a obstacle to set 
-        if (!isSpawning && obstacles is not null) {
-            StartCoroutine(PeriodicSpawn());
-            isSpawning = true;
+        // Spawn Fires
+        if (obstacles is not null) {
+            // Spawn first fire
+            StartCoroutine(FireSpawn(fireIndex1,fireSpawnDelay1, fireUpdateDelay1, SizeUpdateIncremental1, InitialFireSize1, UpdateFireSize1 ));
+            
+            if (fireIndex2 != -1 && fireIndex2 != fireIndex1 && fireIndex2 > 0) {
+                // Spawn second fire, if one is selected
+                StartCoroutine(FireSpawn(fireIndex2, fireSpawnDelay2, fireUpdateDelay2, SizeUpdateIncremental2, InitialFireSize2, UpdateFireSize2));
+            }
         }
     }
 
-    private IEnumerator PeriodicSpawn() {
-        // Wait for at least a 1 second
-        int wait = rnd.Next(1, 4);
-        yield return new WaitForSeconds(wait);
+    private IEnumerator FireSpawn(int index, float spawnDelay, float updateDelay, bool incremental, float fireSize1, float fireSize2) {
+        // Spawn delay
+        yield return new WaitForSeconds(spawnDelay);
         
         // Pick a random obstacle, if not defined
-        if (fireIndex == -1) {
-            fireIndex = rnd.Next(0, obstacles.Length);
-        }
-        GameObject selectedObstacle = obstacles[fireIndex];
+        if (index == -1) { index = rnd.Next(0, obstacles.Length); }
+        GameObject selectedObstacle = obstacles[index];
         selectedObstacle.SetActive(true);
         
-        
         // Change size of fire if was pre-determined
-        if (fireSize != 0) {
-            selectedObstacle.transform.localScale = new Vector3(fireSize, fireSize, fireSize);
+        if (fireSize1 != 0) { selectedObstacle.transform.localScale = new Vector3(fireSize1, fireSize1, fireSize1); }
+        
+        // Delay for the second update
+        if (fireSize2 != 0) {
+            if (incremental) {
+                var num = 20;
+                var delay = updateDelay / num;
+                var fireIncrement = (fireSize2 - fireSize1) / num;
+                for (int i = 0; i < num; i++) {
+                    // ReSharper disable once PossibleLossOfFraction
+                    // small delay
+                    yield return new WaitForSeconds(delay);
+                    var inc = fireSize1 + fireIncrement * (i+1);
+                    selectedObstacle.transform.localScale = new Vector3(inc, inc, inc);
+                }
+            } else {
+                // Wait and update size all at once
+                yield return new WaitForSeconds(updateDelay);
+                selectedObstacle.transform.localScale = new Vector3(fireSize2, fireSize2, fireSize2);
+            }
         }
     }
 }
